647c2d251996f5f9457f304973fa4cc90766da13
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2025-02-24 10:42:14 +0200

    add practice 3

---
+++ practice4.py
@@ -0,0 +1,51 @@
+# print("Hello\tWorld")
+
+
+# print("Hello my little\rsister")
+
+
+# print("Hello\bWorld")
+
+
+# print("Hello\\World")
+
+# print('It\'s a beautiful day')
+# print("He said, \"Hello\"")
+
+
+# text = """This is first line
+# And second line
+# Last third line"""
+
+# song = '''Jingle bells, jingle bells
+# Jingle all the way
+# Oh, what fun it is to ride
+# In a one horse open sleigh'''
+
+# print(text)
+# print(song)
+
+
+# s = "Hi there!"
+
+# start = 0
+# end = 7
+
+# print(s.find("er", start, end)) # 5
+# print(s.find("q")) # -1
+
+
+# s = 'Some words'
+
+# print(s.find("o"))
+# print(s.rfind('o'))
+
+
+text = "hello world"
+result = text.split()
+print(result)  # Виведе: ['hello', 'world']
+
+
+text = "apple,banana,cherry"
+result = text.split(',')
+print(result)  # Виведе: ['apple', 'banana', 'cherry']
